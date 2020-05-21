8e2a9334d44e897f8fe91e37fab84dbc18ecf8b8
Wolf Elkan <wolf.elkan@gmail.com>
2020-05-21 18:00:28 -0400

    Modularized Fields

--- Window.java
+++ Window.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 
+class Field extends JPanel {
+	public Dimension size = new Dimension(0,70);
+	public Field (String title) {
+		JLabel label = new JLabel(title);
+		JTextField field = new JTextField("",20);
+		label.setMinimumSize(size);
+		this.add(label);
+		this.add(field);
+	}
+}
+
 public class Window {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
@@ -11,26 +22,26 @@ public class Window {
 		Dimension labelSize = new Dimension(0,70);
 		Dimension contentBoxSize = new Dimension(FRAME_WIDTH,100);
 
-		JLabel senLabel = new JLabel("From");
-		JTextField senField = new JTextField("",20);
-		senLabel.setMinimumSize(labelSize);
-		JPanel senPanel = new JPanel();
-		senPanel.add(senLabel);
-		senPanel.add(senField);
+		// JLabel senLabel = new JLabel("From");
+		// JTextField senField = new JTextField("",20);
+		// senLabel.setMinimumSize(labelSize);
+		// JPanel senPanel = new JPanel();
+		// senPanel.add(senLabel);
+		// senPanel.add(senField);
 
-		JLabel recLabel = new JLabel("To");
-		JTextField recField = new JTextField("",20);
-		recLabel.setMinimumSize(labelSize);
-		JPanel recPanel = new JPanel();
-		recPanel.add(recLabel);
-		recPanel.add(recField);
+		// JLabel recLabel = new JLabel("To");
+		// JTextField recField = new JTextField("",20);
+		// recLabel.setMinimumSize(labelSize);
+		// JPanel recPanel = new JPanel();
+		// recPanel.add(recLabel);
+		// recPanel.add(recField);
 
-		JLabel subLabel = new JLabel("Subject");
-		JTextField subField = new JTextField("",20);
-		subLabel.setMinimumSize(labelSize);
-		JPanel subPanel = new JPanel();
-		subPanel.add(subLabel);
-		subPanel.add(subField);
+		// JLabel subLabel = new JLabel("Subject");
+		// JTextField subField = new JTextField("",20);
+		// subLabel.setMinimumSize(labelSize);
+		// JPanel subPanel = new JPanel();
+		// subPanel.add(subLabel);
+		// subPanel.add(subField);
 
 		JEditorPane contentBox = new JEditorPane();
 		contentBox.setContentType("text/plain");
@@ -39,13 +50,13 @@ public class Window {
 
 		JButton send = new JButton("Send Email");
 
-		JPanel fields = new JPanel();
-		fields.add(senPanel);
-		fields.add(recPanel);
-		fields.add(subPanel);
-		fields.add(contentBox);
-		fields.add(send);
-		frame.add(fields);
+		JPanel compose = new JPanel();
+		compose.add(new Field("From"));
+		compose.add(new Field("To"));
+		compose.add(new Field("Subject"));
+		compose.add(contentBox);
+		compose.add(send);
+		frame.add(compose);
 
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setTitle("Email Interface");
